a6e785dab65d77c83e36a127c0779adfa0cf9a72
dade80vr <davide@davidepermunian.com>
2017-09-11 09:18:14 +0200

    implemented function control - version 3.2

--- macupgrade.sh
+++ macupgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# macupgrade (https://github.com/dade80vr/macupgrade) - v3.1
+# macupgrade (https://github.com/dade80vr/macupgrade) - v3.2
 #
 # Upgrade HomeBrews, Mac OS and other stuff in one time!
 # Created by Davide Permunian (https://github.com/dade80vr)
@@ -19,7 +19,7 @@ Color_Off='\033[0m'       # Text Reset
 #start banner
 echo -e "$IBlue"
 echo -e "********************************************************"
-echo -e "                   macupgrade v3.1"
+echo -e "                   macupgrade v3.2"
 echo -e " Upgrade HomeBrews, Mac OS and other stuff in one time!"
 echo -e "      https://github.com/dade80vr/macupgrade"
 echo -e "                  --help for manual"
@@ -74,39 +74,63 @@ if [ "$own_usr" != "$(whoami)" ]; then
 	if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
 fi
 
-#upgrading HomeBrew and Cask
-echo -en "${IYellow}*** Step 1/7 *** Run HomeBrew update.. ${Color_Off}"
-brew update > $runmode
-if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
-echo -en "${IYellow}*** Step 2/7 *** Run HomeBrew upgrade.. ${Color_Off}"
-brew upgrade > $runmode
-if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+if ! [ -x "$(command -v brew)" ]
+then
+  echo -e "${IRed}*** SKIP Steps 1-2-3 *** BREW not installed${Color_Off}"
+else
+	#upgrading HomeBrew and Cask
+	echo -en "${IYellow}*** Step 1/7 *** Run HomeBrew update.. ${Color_Off}"
+	brew update > $runmode
+	if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+	echo -en "${IYellow}*** Step 2/7 *** Run HomeBrew upgrade.. ${Color_Off}"
+	brew upgrade > $runmode
+	if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+	#homeBrew + Cask cache cleaning
+	echo -en "${IYellow}*** Step 3/7 *** Clear HomeBrew cache.. ${Color_Off}"
+	brew cleanup --force > $runmode
+	brew cask cleanup > $runmode
+	rm -f -r /Library/Caches/Homebrew/* > $runmode
+	if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+fi
 
-#homeBrew + Cask cache cleaning
-echo -en "${IYellow}*** Step 3/7 *** Clear HomeBrew cache.. ${Color_Off}"
-brew cleanup --force > $runmode
-brew cask cleanup > $runmode
-rm -f -r /Library/Caches/Homebrew/* > $runmode
-if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+if ! [ -x "$(command -v apm)" ]
+then
+  echo -e "${IRed}*** SKIP Step 4 *** APM not installed${Color_Off}"
+else
+	#Atom upgrade without confirm
+	echo -en "${IYellow}*** Step 4/7 *** Run Atom upgrade.. ${Color_Off}"
+	apm upgrade -c false > $runmode
+	if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+fi
 
-#Atom upgrade without confirm
-echo -en "${IYellow}*** Step 4/7 *** Run Atom upgrade.. ${Color_Off}"
-apm upgrade -c false > $runmode
-if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+if ! [ -x "$(command -v npm)" ]
+then
+  echo -e "${IRed}*** SKIP Step 5 *** NPM not installed${Color_Off}"
+else
+	#NPM upgrade
+	echo -en "${IYellow}*** Step 5/7 *** Run NPM update.. ${Color_Off}"
+	npm update -g > $runmode
+	if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+fi
 
-#NPM upgrade
-echo -en "${IYellow}*** Step 5/7 *** Run NPM update.. ${Color_Off}"
-npm update -g > $runmode
-if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+if ! [ -x "$(command -v mas)" ]
+then
+  echo -e "${IRed}*** SKIP Step 6 *** MAS not installed${Color_Off}"
+else
+	#upgrading MAS
+	echo -en "${IYellow}*** Step 6/7 *** Check Mac App Store upgrades: ${IPurple}insert your Apple ID pass if required.. ${Color_Off}"
+	mas upgrade > $runmode
+	if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
+fi
 
-#upgrading MAS
-echo -en "${IYellow}*** Step 6/7 *** Check Mac App Store upgrades: ${IPurple}insert your Apple ID pass if required.. ${Color_Off}"
-mas upgrade > $runmode
-if [ $runmode == "/dev/null" ]; then echo -e "${IGreen}done!${Color_Off}"; else echo -e ""; fi
-
-#system updates
-echo -e "${IYellow}*** Step 7/7 *** Check Mac OS upgrades: ${IPurple}insert your super-user pass if required.. ${Color_Off}"
-sudo softwareupdate -iva > $runmode
+if ! [ -x "$(command -v softwareupdate)" ]
+then
+  echo -e "${IRed}*** SKIP Step 7 *** SOFTWAREUPDATE not installed${Color_Off}"
+else
+	#system updates
+	echo -e "${IYellow}*** Step 7/7 *** Check Mac OS upgrades: ${IPurple}insert your super-user pass if required.. ${Color_Off}"
+	sudo softwareupdate -iva > $runmode
+fi
 
 #finish!
 echo -e "${IGreen}***** End! *****${Color_Off}"
